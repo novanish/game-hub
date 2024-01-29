b8f7baf2af41a594b6083e07382b77f66a15f164
Anish Chhetri <anishchhetri196@gmail.com>
2024-01-29 09:15:57 +0545

    Create a genric data fetching hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { apiClient } from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export function useFetch<T>(endpoint: string) {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchResponse<T>>(endpoint, {
+        signal: controller.signal,
+      })
+      .then((response) => {
+        setData(response.data.results);
+        // setTimeout(() => setIsLoading(false), 2000);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+
+        console.error(error);
+        setError(error.message);
+        setIsLoading(false);
+      });
+
+    return () => {
+      controller.abort();
+    };
+  }, [endpoint]);
+
+  return { data, error, isLoading };
+}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { apiClient } from "../services/api-client";
-import { CanceledError } from "axios";
+import { useFetch } from "./useFetch";
 
 export interface Platform {
   id: number;
@@ -25,40 +23,8 @@ export interface Game {
   metacritic: number;
 }
 
-interface FetchGameResponse {
-  count: number;
-  results: Game[];
-}
-
 export function useGames() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const { data: games, ...rest } = useFetch<Game>("/games");
 
-  useEffect(() => {
-    setIsLoading(true);
-    const controller = new AbortController();
-
-    apiClient
-      .get<FetchGameResponse>("/games", { signal: controller.signal })
-      .then((response) => {
-        setGames(response.data.results);
-        console.log(response.data.results);
-        setTimeout(() => setIsLoading(false), 2000);
-        // setIsLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-
-        console.error(error);
-        setError(error.message);
-        setIsLoading(false);
-      });
-
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
-  return { games, error, isLoading };
+  return { games, ...rest };
 }
